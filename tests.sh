1e5d62d3f30f520d664e45fa9c19345f27be5b4f
Orie Steele <orie@transmute.industries>
2022-04-24 11:57:42 -0500

    chore: cleanup

--- tests.sh
+++ tests.sh
@@ -5,6 +5,6 @@
 # curl -sX GET "https://www.federalregister.gov/api/v1/documents/2022-08590.json" -H "accept: */*" | jq
 
 
-curl -sX GET "https://www.federalregister.gov/documents/full_text/text/2022/04/25/2022-08777.txt" \
-    -H "accept: */*" \
-    | tail -n +8 | head -n 40
+# curl -sX GET "https://www.federalregister.gov/documents/full_text/text/2022/04/25/2022-08777.txt" \
+#     -H "accept: */*" \
+#     | tail -n +8 | head -n 40
